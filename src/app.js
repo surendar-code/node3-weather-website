7a6bd95df6d98d8bbedbf8f6c17a156c7c071478
Surendar Ram <surendar.ramamoorthy@gmail.com>
2020-10-25 12:32:21 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,112 @@
+const forecast= require('./utils/forecast')
+const geocode= require('./utils/geocode')
+
+
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+
+const { title } = require('process')
+const app = express()
+
+//Define paths for express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname,'../templates/views')
+const partialPath = path.join(__dirname,'../templates/partials')
+
+//Setup handlebar engines and view location
+app.set('view engine','hbs')
+app.set('views',viewPath)
+hbs.registerPartials(partialPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirPath))
+
+app.get('',(req,res) => {
+    res.render('index', {
+        title : 'Weather App',
+        name : 'Surendar Ram'
+    })
+})
+
+app.get('/about',(req,res) => {
+    res.render('about',{
+        title:'About Me',
+        name : 'Surendar Ram'
+
+    })
+})
+
+app.get('/help',(req,res) => {
+    res.render('help',{
+        title: 'Help',
+        helpMsg : 'This is a Help Page',
+        name:'Surendar Ram'
+    })
+})
+
+app.get('/help/*',(req,res) => {
+   res.render('404',{
+       title: '404',
+       msg : 'Help article not found !',
+       name : 'Surendar Ram'
+   })
+})
+
+app.get('/weather',(req,res) => {
+
+    const address =req.query.address
+
+    if(!address)
+    {
+        return res.send('Enter the address to fetch weather information !')
+    }
+   geocode(address,(error,{latitude,longitude,location}={}) => {
+       if(error){
+           return res.send({error})
+       }
+  
+      forecast(latitude,longitude, (error, ForecastData) => {
+          if(error){
+              return res.send(error);
+          }
+          res.send({              
+            location,
+            ForecastData: ForecastData,
+            address
+        });
+            
+        })
+  }) 
+
+    // res.send({
+    //     address : address,
+    //     forecast: 'Cloudy',
+    //     location : 'Madurai'
+  })
+
+app.get('/products',(req,res) => {
+
+    if(!req.query.search){
+        console.log('Enter search term ')
+        return res.send('Enter search term ')
+        
+    }
+    console.log(req.query.search);
+    res.send({
+        products:[]
+    })
+})
+app.get('*',(req,res) => {
+    res.render('404',{
+        title: '404',
+        msg: 'Page not found !',
+        name : 'Surendar Ram'
+    })
+})
+
+
+app.listen(3000,() => {
+    console.log('Server is up on port 3000');
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (longtitude,latitude,callback) =>{
+    const url = 'http://api.weatherstack.com/current?access_key=72d2f56817750d839eb83984d7726791&query='+longtitude+','+latitude
+
+    request({url, json:true},(error,{body}={}) => {
+        if(error){
+            callback('Unable to connect to WeatherStack!',undefined)
+        }else if(body.error){
+            callback('Unable to find the Location',undefined)
+        }else{
+            callback(undefined,'weather is '+body.current.weather_descriptions+' .It is ' +body.current.temperature +' degrees and it feels like '+body.current.feelslike+' degrees')
+        }
+    })
+
+}
+
+module.exports = forecast
